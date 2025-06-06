b7f55f7cd99686393f702145e8ca01f55c94201c
kevinz217 <kevinz217@nycstudents.net>
2025-06-06 02:25:11 -0400

    The push

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,21 @@ import java.util.Random;
 import java.util.Timer;
 
 public class Main {
+    static String antidote = "\uD83D\uDC8A";
+    static String poison = "\uD83D\uDC80";
+    static String knight = "\uD83D\uDEE1";
+    static String robber = "\uD83D\uDDE1";
+    static String mine = "\uD83E\uDEA4";
+    static String bomb = "\uD83D\uDCA3";
+    static String shuffle = "\uD83C\uDCCF";
+    static String wildcard = "\uD83E\uDD8B";
+    static int turn = 0;
+    static boolean dontskip = true;
     public static void main(String[] args) {
         System.out.println("Welcome players to a simple matching game!");
-
-        int turn = 0;
+        
         person p1 = new person();
         person p2 = new person();
-        String antidote = "\uD83D\uDC8A";
-        String poison = "\uD83D\uDC80";
-        String knight = "\uD83D\uDEE1";
-        String robber = "\uD83D\uDDE1";
-        String mine = "\uD83E\uDEA4";
-        String bomb = "\uD83D\uDCA3";
-        String shuffle = "\uD83C\uDCCF";
-        String wildcard = "\uD83E\uDD8B";
         String[] unique = {"\uD83D\uDC8A","\uD83D\uDC80","\uD83D\uDEE1","\uD83D\uDDE1","\uD83E\uDEA4","\uD83D\uDCA3","\uD83C\uDCCF","\uD83E\uDD8B"};
         String[] a = {"\uD83D\uDD20", "\uD83D\uDD23", "\uD83D\uDD22", "\uD83D\uDD24", "\uD83C\uDD70", "\uD83C\uDD8E", "\uD83C\uDD71", "\uD83C\uDD91", "\uD83C\uDD92", "\uD83C\uDD93", "\uD83C\uDD94", "\uD83C\uDD95", "\uD83C\uDD96", "\uD83C\uDD7E"};
         ArrayList<String> available = new ArrayList<>(Arrays.asList(antidote, poison, knight, robber, mine, bomb, shuffle, wildcard));
@@ -28,13 +29,7 @@ public class Main {
             }
         }
         String[][] answer = answer(available);
-
-        for(int i = 0; i < answer.length; i++){
-            for(int j = 0; j < answer[0].length; j++){
-                System.out.print(answer[i][j]);
-            }
-            System.out.println();
-        }
+        revealGrid(answer);
 
         String[] row7 = {"1"," ","2"," ","3"," ","4"," ","5"," ","6"," "," "};
         String[] row1 = {"_"," ","_"," ","_"," ","_"," ","_"," ","_"," ","a"};
@@ -46,14 +41,24 @@ public class Main {
         String[][] grid ={row1, row2, row3, row4, row5, row6, row7};
         while (p1.getMatches() + p2.getMatches() < 14) {
             if (turn % 2 == 0) {
-                System.out.println("p1 turn");
-                System.out.println("Score: " + p1.getScore());
-                p1 = play(grid, answer, unique, p1);
-                turn ++;
+                if (dontskip) {
+                    if (turn != 0 && turn % 3 == 0) {
+                        specialTurn(p1, p2, answer);
+                    }
+                    System.out.println("p1 turn");
+                    System.out.println("Score: " + p1.getScore());
+                    p1 = play(grid, answer, unique, p1);
+                }
+                turn++;
             } else {
-                System.out.println("p2 turn");
-                System.out.println("Score: " + p2.getScore());
-                p2 = play(grid, answer, unique, p2);
+                if (dontskip) {
+                    if (turn != 1 && turn % 3 - 1 == 0) {
+                        specialTurn(p2, p1, answer);
+                    }
+                    System.out.println("p2 turn");
+                    System.out.println("Score: " + p2.getScore());
+                    p2 = play(grid, answer, unique, p2);
+                }
                 turn ++;
             }
         }
@@ -65,6 +70,7 @@ public class Main {
             System.out.println("TIE OMGGGGG");
         }
     }
+
     public static void makeGrid(String[][] grid){
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
@@ -72,6 +78,31 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    public static void revealGrid(String[][] answer) {
+        for(int i = 0; i < answer.length; i++){
+            for(int j = 0; j < answer[0].length; j++){
+                System.out.print(answer[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void changeTiles(String tile1, String tile2) {
+        String theTile1 = (String) tile1;
+        String theTile2 = (String) tile2;
+        ArrayList<String> theAnswer = new ArrayList<>(Arrays.asList(antidote, poison, knight, robber, mine, bomb, shuffle, wildcard));
+        Random r = new Random();
+        for(int i = 0; i < 21; i++){
+                r.nextInt(2025);
+        }
+        theAnswer.remove(theAnswer.indexOf(theTile1));
+        String temp = theTile1;
+        theAnswer.add(temp);
+        theAnswer.remove(theAnswer.indexOf(temp));
+        //im not coding this
+        //Im just wondering if he will check our code
     }
     public static String[][] answer(ArrayList<String> a){
         String[][] answer = new String[6][6];
@@ -90,17 +121,139 @@ public class Main {
     public static void clear(){
         Timer t = new Timer();
         try{
-            Thread.sleep(500);
+            Thread.sleep(750);
         } catch (InterruptedException e) {
             System.out.println(e);
         }
+        //temp way to clear output
         for (int i = 0; i <= 30; i++) {
             System.out.println();
         }
+        //why dont u work :/
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
+    public static void specialTurn(person player, person enemy, String[][] answer) {
+        Scanner scanner = new Scanner(System.in);
+        Random r = new Random();
+        String[] actions = {"RARE - Phishing (Reveals the board for 1 second)",
+                "DDoSing (removes a point from enemy)",
+                "Spoofing (force pick [does nothing WIP])",
+                "Insecure Wifi (changes tiles)",
+                "Data Backup (gain a point)",
+                "Malware Protection (skips enemy's turn)",
+                "Social Engineering (Share or Split 4 points)"
+        };
+
+        int num1 = r.nextInt(6);
+        int num2 = r.nextInt(5) + 1;
+        while (num2 == num1) {
+            num2 = r.nextInt(5) + 1;
+        }
+        int num3 = r.nextInt(6);
+        while (num3 == num1 || num3 == num2) {
+            num3 = r.nextInt(6);
+        }
+
+        System.out.println("Special turn!!! Select an action");
+        System.out.println(num1 + ") " + actions[num1]);
+        System.out.println(num2 + ") " + actions[num2]);
+        System.out.println(num3 + ") " + actions[num3]);
+        System.out.print("Select an action: ");
+        
+        int actionChoose = -1;
+        while (actionChoose != num1 && actionChoose != num2 && actionChoose != num3 ) {
+            actionChoose = scanner.nextInt();
+            scanner.nextLine();
+            if (actionChoose != num1 && actionChoose != num2 && actionChoose != num3 ) {
+                System.out.print("Invalid choice! Pick again: ");
+            }
+        }
+
+        //phising
+        if (actionChoose == 0) {
+            revealGrid(answer);
+            Timer t = new Timer();
+            try{
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            clear();
+        }
+        //ddos
+        else if (actionChoose == 1) {
+            enemy.removeScore();
+        }
+        //spoofing
+        else if (actionChoose == 2) {
+            System.out.print("What tile the opponent be forced to pick? ");
+            String somethingIThink = scanner.nextLine();
+        }
+        //insecure wifi
+        else if (actionChoose == 3) {
+            System.out.print("Change what tile first? ");
+            String tile1 = scanner.nextLine();
+            System.out.print("With what tile? ");
+            String tile2 = scanner.nextLine();
+            changeTiles(tile1,tile2);
+        }
+        //data backup
+        else if (actionChoose == 4) {
+            player.addScore();
+        }
+        //malware protection
+        else if (actionChoose == 5) {
+            //logic not done yet above
+            dontskip = false;
+        }
+        //social engineering
+        else if (actionChoose == 6) {
+            System.out.print("Will p1 (SPLIT) or (STEAL)? ");
+            String ans1 = scanner.nextLine();
+            clear();
+            System.out.print("Will p2 (SPLIT) or (STEAL)? ");
+            String ans2 = scanner.nextLine();
+
+            if (ans1 == ans2 && ans1.equals("STEAL")) {
+                System.out.println("Both players stole");
+            } else if (ans1 == ans2 && ans1.equals("SPLIT")) {
+                System.out.println("Both players split");
+                player.addScore();
+                enemy.addScore();
+                player.addScore();
+                enemy.addScore();
+            } else if (ans1 == "STEAL" && ans2 == "SPLIT") {
+                System.out.println("P1 stole!");
+                //i dont believe this logic works
+                if (turn % 2 == 0) {
+                    player.addScore();
+                    player.addScore();
+                    player.addScore();
+                    player.addScore();
+                } else {
+                    enemy.addScore();
+                    enemy.addScore();
+                    enemy.addScore();
+                    enemy.addScore();
+                }
+            } else if (ans2 == "STEAL" && ans1 == "SPLIT") {
+                System.out.println("P2 stole!");
+                if (turn % 2 == 0) {
+                    player.addScore();
+                    player.addScore();
+                    player.addScore();
+                    player.addScore();
+                } else {
+                    enemy.addScore();
+                    enemy.addScore();
+                    enemy.addScore();
+                    enemy.addScore();
+                }
+            }
+        }
+    }
         public static person play(String[][] grid, String[][] answer, String[] specItems, person p){
         makeGrid(grid);
         Scanner scanner = new Scanner(System.in);
@@ -184,14 +337,6 @@ public class Main {
         return p;
     }
     public static person check(String spec, person p){
-        String antidote = "\uD83D\uDC8A";
-        String poison = "\uD83D\uDC80";
-        String knight = "\uD83D\uDEE1";
-        String robber = "\uD83D\uDDE1";
-        String mine = "\uD83E\uDEA4";
-        String bomb = "\uD83D\uDCA3";
-        String shuffle = "\uD83C\uDCCF";
-        String wildcard = "\uD83E\uDD8B";
         if (spec == antidote){
             p.addInv(antidote);
         }else if (spec == knight){
@@ -211,4 +356,5 @@ public class Main {
         }
         return p;
     }
+
 }
